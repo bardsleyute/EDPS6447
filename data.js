50f9f800dd8bdd3e9089ce449f23b067995f0334
bardsleyute <59671301+bardsleyute@users.noreply.github.com>
2020-06-02 19:41:44 -0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,23 @@
+var array_questions = [
+    {
+        statement: "Michael Jordan scored 38 points in his flu game against the Utah Jazz in game 5 of the 1997 NBA Finals.",
+        topic: "trivia",
+        sub_topic: "points",
+        answer_key: "option1",
+        hint: "https://www.nba.com/top-nba-finals-moments-michael-jordan-flu-game-game-5-1997-finals#:~:text=The%20Play%3A%20So%20sick%20and,clutch%20performer%20and%20relentless%20competitor.",
+        video: (display: "true", src="https://www.youtube.com/embed/-WWvVVXOiOE"),
+    },
+    {
+        statement: "Michael Jordan averaged 22.3 points per game against the Utah Jazz in the 1997 NBA Finals.",
+        topic: "trivia",
+        sub_topic: "points",
+        answer_key: "option2",
+        hint: "https://www.basketball-reference.com/playoffs/1997-nba-finals-jazz-vs-bulls.html",
+    },
+];
+
+var array_answers = [];
+
+console.log(array_questions);
+console.log(array_answers);
+console.log(array_questions[1].statement);
