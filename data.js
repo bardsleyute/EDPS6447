b1a2380c047ac5af0c47ac4380271d593cee4f4a
bardsleyute <59671301+bardsleyute@users.noreply.github.com>
2020-07-29 13:40:54 -0600

    Add files via upload

--- data.js
+++ data.js
@@ -1,23 +1,44 @@
 var array_questions = [
+	{
+		statement: "Michael Jordan averaged 33.5 points a game in his final season as a Chicago Bull.",
+		topic: "NBA Trivia",
+		sub_topic: "Michael Jordan",
+		answer_key: "option1",
+		hint: "https://www.google.com/search?q=How+many+points+did+Michael+Jordan+average+in+his+final+season+on+the+chicago+bulls%3F&rlz=1C1GCEU_enUS866US868&oq=How+many+points+did+Michael+Jordan+average+in+his+final+season+on+the+chicago+bulls%3F&aqs=chrome..69i57.17959j1j7&sourceid=chrome&ie=UTF-8",
+		label: ["True", "False", "Maybe"],
+	},
+	{
+		statement: "The most points that Michael Jordan scored in a single game was 60.",
+		topic: "NBA Trivia",
+		sub_topic: "Michael Jordan",
+		answer_key: "option2",
+		hint: "https://www.youtube.com/embed/3M2NCcSxKLY",
+		label: ["True", "False", "Maybe"],
+	},
     {
-        statement: "Michael Jordan scored 38 points in his flu game against the Utah Jazz in game 5 of the 1997 NBA Finals.",
-        topic: "trivia",
-        sub_topic: "points",
-        answer_key: "option1",
-        hint: "https://www.nba.com/top-nba-finals-moments-michael-jordan-flu-game-game-5-1997-finals#:~:text=The%20Play%3A%20So%20sick%20and,clutch%20performer%20and%20relentless%20competitor.",
-        video: (display: "true", src="https://www.youtube.com/embed/-WWvVVXOiOE"),
+        statement: "Michael Jordan's highest points per game average for an NBA finals was against the Utah Jazz in the 1997 NBA Finals.",
+        topic: "NBA Trivia",
+        sub_topic: "Michael Jordan",
+        answer_key: "option2",
+        hint: "https://www.michaeljordansworld.com/stats_finals.htm",
+        label: ["True", "False", "Maybe"],
     },
     {
-        statement: "Michael Jordan averaged 22.3 points per game against the Utah Jazz in the 1997 NBA Finals.",
-        topic: "trivia",
-        sub_topic: "points",
-        answer_key: "option2",
-        hint: "https://www.basketball-reference.com/playoffs/1997-nba-finals-jazz-vs-bulls.html",
+        statement: "Michael Jordan is the greatest NBA player of all time.",
+        topic: "NBA Trivia",
+        sub_topic: "Michael Jordan",
+        answer_key: "option3",
+        hint: "https://www.espn.com/nba/story/_/id/29021691/nba-goat-debate-big-questions-michael-jordan-greatest-players-ever",
+        label: ["True", "False", "Maybe"],
+    },
+    {
+        statement: "Michael Jordan won 6 NBA Championships while playing for the Chicago Bulls.",
+        topic: "NBA Trivia",
+        sub_topic: "Michael Jordan",
+        answer_key: "option1",
+        hint: "https://www.sportingnews.com/us/nba/news/michael-jordan-championships-breakdown/1vqo0iif16sq51lawsaxtitciw",
+        label: ["True", "False", "Maybe"],
     },
 ];
 
 var array_answers = [];
-
-console.log(array_questions);
-console.log(array_answers);
-console.log(array_questions[1].statement);
--- variable.js
+++ variable.js
@@ -1,18 +1,21 @@
-console.log("Hello World!");
-
 // Sections in the HTML CSS Grid
-var sec_landing = document.getElementById("sec_landing");
+var sec_landing = document.getElementById("sec_landing"),
 	sec_quiz = document.getElementById("sec_quiz");
 
 // Containers to display feedback
 var cont_success = document.getElementById("cont_success"),
-	cont_danger = document.getElementById("cont_danger"),
-    iframe_video = document.getElementById("iframe_video");
+	cont_danger = document.getElementById("cont_danger");
 
 // Inputs - textbox or radio buttons
 var input_username = document.getElementById("input_username"),
 	input_1 = document.getElementById("input_1"),
-	input_2 = document.getElementById("input_2");
+	input_2 = document.getElementById("input_2"),
+	input_3 = document.getElementById("input_3"),
+	text_input1 = document.getElementById("text_input1"),
+	text_input2 = document.getElementById("text_input2"),
+	text_input3 = document.getElementById("text_input3");
+console.log(input_3);
+console.log(text_input3);
 
 // Buttons
 var btn_start = document.getElementById("btn_start"),
@@ -37,23 +40,3 @@ var counter = 0,
 	end_time,
 	username,
 	lrs;
-
-/*
-console.log(input_username);
-console.log(btn_start);
-console.log(btn_tutorial);
-console.log(text_statement);
-console.log(input_1);
-console.log(input_2);
-console.log(cont_success);
-console.log(cont_danger);
-console.log(btn_submit);
-console.log(text_question_id);
-console.log(text_question_total);
-console.log(text_question_correct);
-console.log(text_question_incorrect);
-console.log(btn_previous);
-console.log(btn_hint);
-console.log(btn_next);
-console.log(text_agent_dialogue);
-*/
